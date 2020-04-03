906651e22988485a124aeed2410d08b308a411ac
Jose Atlin <36805024+JoseAtlin@users.noreply.github.com>
2020-04-03 08:34:25 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -20,59 +20,56 @@ def start():
 def play(attempts,length,word,user):
 	guess = []
 	symbols = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','/','`','~']
-	while(1):
-		count = 0
-		while(attempts > 0):
-			flag = 0
-			print(("Word : "+' '.join(user)).center(140))
-			print("Attempts Remaining : {}".format(attempts))
-			if len(guess) == 0:
-				print("Previous Guesses : NIL")
-			else:
-				print("Previous Guesses : {}".format(' '.join(guess)))
+	count = 0
+	while(attempts > 0):
+		flag = 0
+		print(("Word : "+' '.join(user)).center(140))
+		print("Attempts Remaining : {}".format(attempts))
+		if len(guess) == 0:
+			print("Previous Guesses : NIL")
+		else:
+			print("Previous Guesses : {}".format(' '.join(guess)))
 
-			letter = input("Choose a letter : ").lower()
-			if len(letter) > 1:
-				print("choose only a letter, No words".center(140,'-'))
-				continue
-			if letter.isdigit():
-				print("Choose only a letter, No Numbers".center(140,'-'))
-				continue
-			if letter in symbols:
-				print("Choose only a letter, No symbols".center(140,'-'))
-				continue
+		letter = input("Choose a letter : ").lower()
+		if len(letter) > 1:
+			print("choose only a letter, No words".center(140,'-'))
+			continue
+		if letter.isdigit():
+			print("Choose only a letter, No Numbers".center(140,'-'))
+			continue
+		if letter in symbols:
+			print("Choose only a letter, No symbols".center(140,'-'))
+			continue
 
-			for i in range(len(word)):
-				if word[i] == letter:
-					user[i] = letter
-					guess.append(letter)
-					print("You guessed right..!\n".center(140))
-					flag = 1
-					count += 1
-					break
-			if flag == 0:
+		for i in range(len(word)):
+			if word[i] == letter:
+				user[i] = letter
 				guess.append(letter)
-				print("Ooops.. {} is not in the word\n".format(letter).center(140))	
-
-			if count == len(word):
-				print("You've Won !! Great Job...".center(140,'-'))
-				print('\n')
+				print("You guessed right..!\n".center(140))
+				flag = 1
+				count += 1
 				break
+		if flag == 0:
+			guess.append(letter)
+			print("Ooops.. {} is not in the word\n".format(letter).center(140))	
 
-			if attempts == 1:
-				print("Ooops you are out of moves :(".center(140,'-'))
-				print("Better luck next time\n".center(140))
-				print("Word was : {}".format(''.join(word)).rjust(140))
-			attempts -= 1
-
-		print("Do you wish to play again?".center(140))
-		print("1.Restart  2.End Game\n".center(140))
-		ch = int(input("Enter your Choice : "))
-		print('\n')
-		if ch == 1:
-			start()
-		elif ch == 2:
+		if count == len(word):
+			print("You've Won !! Great Job...".center(140,'-'))
+			print('\n')
 			break
+
+		if attempts == 1:
+			print("Ooops you are out of moves :(".center(140,'-'))
+			print("Better luck next time\n".center(140))
+			print("Word was : {}".format(''.join(word)).rjust(140))
+		attempts -= 1
+
+	print("Do you wish to play again?".center(140))
+	print("1.Restart  2.End Game\n".center(140))
+	ch = int(input("Enter your Choice : "))
+	print('\n')
+	if ch == 1:
+		start()
 
 pattern = [('.|.'*(2*i + 1)).center(140,'-') for i in range(5//2)]
 print('\n'.join(pattern + ['WELCOME TO HANGMAN'.center(140, '-')] + pattern[::-1]))
