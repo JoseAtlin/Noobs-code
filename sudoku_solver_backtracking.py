dd1496119962be4efbcd917c5a0220b80f744038
Jose Atlin <36805024+JoseAtlin@users.noreply.github.com>
2020-04-12 15:26:12 +0530

    Update sudoku_solver_backtracking.py

--- sudoku_solver_backtracking.py
+++ sudoku_solver_backtracking.py
@@ -13,7 +13,7 @@ def printGrid():
 	print(' '.join(str(matrix[7])).replace('[','').replace(']','').center(140))
 	print(' '.join(str(matrix[8])).replace('[','').replace(']','').center(140))
 
-def isEmpty(matrix,l): 				#to check the spots where we need to fill numbers
+def isEmpty(l): 				#to check the spots where we need to fill numbers
 	for i in range(9): 
 		for j in range(9): 
 			if(matrix[i][j] == 0): 
@@ -22,40 +22,42 @@ def isEmpty(matrix,l): 				#to check the spots where we need to fill numbers
 				return True
 	return False
 
-def usedRow(matrix,row,check): 			#to check if the number is anywhere in the row
+def usedRow(row,check): 			#to check if the number is anywhere in the row
     for i in range(9): 
         if(matrix[row][i] == check): 
             return True
     return False
   
-def usedCol(matrix,col,check): 			#to check if the number is anywhere in the column
+def usedCol(col,check): 			#to check if the number is anywhere in the column
     for i in range(9): 
         if(matrix[i][col] == check): 
             return True
     return False
   
-def usedBox(matrix,row,col,check): 		#to check if the number if anywhere in the box
+def usedBox(row,col,check): 			#to check if the number if anywhere in the box
+    row -= row % 3	
+    col -= col % 3
     for i in range(3): 
         for j in range(3): 
             if(matrix[i+row][j+col] == check): 
                 return True
     return False
   
-def isSafe(matrix,row,col,num): 		#checks if the element to insert is not in any of the column, row, box
-    return not usedRow(matrix,row,num) and not usedCol(matrix,col,num) and not usedBox(matrix,row - row%3,col - col%3,num) 
+def isSafe(row,col,num): 			#checks if the element to insert is not in any of the column, row, box
+    return not usedRow(row,num) and not usedCol(col,num) and not usedBox(row,col,num) 
   
  
-def solve(matrix): 
+def solve(): 
 	l=[0]*2					#keep track of the row and colums of sudoku matrix
-	if(not isEmpty(matrix,l)): 
+	if(not isEmpty(l)): 
 		return True			#if there is not empty spots left, which means solution found
 
 	row = l[0] 
 	col = l[1]
 	for put in range(1,10):			#chooses a number from 1 - 9
-		if(isSafe(matrix,row,col,put)): 
+		if(isSafe(row,col,put)): 
 			matrix[row][col] = put 	#puts the number that is not present in row, column, box
-			if(solve(matrix)): 	
+			if(solve()): 	
 				return True	#moves to the next spot
 		matrix[row][col] = 0
 
@@ -73,7 +75,7 @@ matrix = array.reshape(9,9)
 print('\n')
 print("The given Sudoku Grid :\n".center(140))
 printGrid()
-if solve(matrix):
+if solve():
 	print("\n")
 	print("Solved Sudoku :".center(140))
 	printGrid()
