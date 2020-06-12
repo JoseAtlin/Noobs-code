e4ddddb3283948de0664a80ad3ebad151207315e
Jose Atlin <36805024+JoseAtlin@users.noreply.github.com>
2020-06-12 09:09:43 +0530

    Update messaging_sockets.py

--- messaging_sockets.py
+++ messaging_sockets.py
@@ -11,66 +11,73 @@ def start():
         thread = threading.Thread(target=client_server, args=(conn, addr))
         thread.start()
         conn_ip_dict[conn] = addr
-        print("[ACTIVE CONNECTIONS] : ", threading.activeCount() - 1)
+        print("[ACTIVE CONNECTIONS] : ", len(conn_ip_dict))
 
 
 def client_server(conn, addr):
     print("[NEW CONNECTION] : {}".format(addr))
+    length = conn.recv(4).decode('utf-8')
+    client = conn.recv(int(length)).decode('utf-8')
+    conn_name_dict[conn] = client
+    online.add(client)
     while 1:
         msg_length = conn.recv(16).decode('utf-8')
         if msg_length:
             message_details = []                                            # [sender, receiver, message, time]
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode('utf-8')
+            msg = conn.recv(int(msg_length)).decode('utf-8')
             sender_length = conn.recv(4).decode('utf-8')
-            sender_length = int(sender_length)
-            sender = conn.recv(sender_length).decode('utf-8')
+            sender = conn.recv(int(sender_length)).decode('utf-8')
             receiver_length = conn.recv(4).decode('utf-8')
-            receiver_length = int(receiver_length)
-            receiver = conn.recv(receiver_length).decode('utf-8')
+            receiver = conn.recv(int(receiver_length)).decode('utf-8')
             time = datetime.now().strftime("%I:%M:%P")
             message_details.extend((sender, receiver, msg, time))
             server_messages.append(message_details)
-            conn_name_dict[conn] = sender
             if msg == "DISCONNECT":
                 print("[{}] : DISCONNECTED".format(addr))
                 conn.close()
                 del conn_name_dict[conn]
+                online.remove(sender)
                 break
             print(message_details)
-            for socks in conn_name_dict:
-                if conn_name_dict[socks] != sender:
-                    if receiver == "EVERYONE":
-                        socks.send('1'.encode('utf-8'))
-                        s_l = len(sender)
-                        s_l = str(s_l).encode('utf-8')
+            server_send(conn, sender, receiver, msg, time)
+
+
+def server_send(conn, sender, receiver, msg, time):
+    if receiver == "EVERYONE" or receiver in online:
+        for socks in conn_name_dict:
+            if conn_name_dict[socks] != sender:
+                if receiver == "EVERYONE":
+                    socks.send('1'.encode('utf-8'))
+                    s_l = str(len(sender)).encode('utf-8')
+                    s_l += b' ' * (4 - len(s_l))
+                    socks.send(s_l)
+                    socks.send(sender.encode('utf-8'))
+                    m_l = len(msg)
+                    m_l = str(m_l).encode('utf-8')
+                    m_l += b' ' * (16 - len(m_l))
+                    socks.send(m_l)
+                    socks.send(msg.encode('utf-8'))
+                    socks.send(time.encode('utf-8'))
+                else:
+                    if conn_name_dict[socks] == receiver:
+                        socks.send('2'.encode('utf-8'))
+                        s_l = str(len(sender)).encode('utf-8')
                         s_l += b' ' * (4 - len(s_l))
                         socks.send(s_l)
                         socks.send(sender.encode('utf-8'))
-                        m_l = len(msg)
-                        m_l = str(m_l).encode('utf-8')
+                        m_l = str(len(msg)).encode('utf-8')
                         m_l += b' ' * (16 - len(m_l))
                         socks.send(m_l)
                         socks.send(msg.encode('utf-8'))
                         socks.send(time.encode('utf-8'))
-                    else:
-                        if conn_name_dict[socks] == receiver:
-                            socks.send('2'.encode('utf-8'))
-                            s_l = len(sender)
-                            s_l = str(s_l).encode('utf-8')
-                            s_l += b' ' * (4 - len(s_l))
-                            socks.send(s_l)
-                            socks.send(sender.encode('utf-8'))
-                            m_l = len(msg)
-                            m_l = str(m_l).encode('utf-8')
-                            m_l += b' ' * (16 - len(m_l))
-                            socks.send(m_l)
-                            socks.send(msg.encode('utf-8'))
-                            socks.send(time.encode('utf-8'))
-                else:
-                    socks.send('3'.encode('utf-8'))                    
-                    socks.send("Message Delivered".encode('utf-8'))
-                    socks.send(time.encode('utf-8'))
+            else:
+                socks.send('3'.encode('utf-8'))
+                socks.send("Message Delivered".encode('utf-8'))
+                socks.send(time.encode('utf-8'))
+    else:
+        conn.send('4'.encode('utf-8'))
+        conn.send("Message NOT Delivered".encode('utf-8'))
+        conn.send(time.encode('utf-8'))
 
 
 def client_receive():
@@ -78,16 +85,19 @@ def client_receive():
         person = c.recv(1).decode('utf-8')
         if person == '1' or person == '2':
             s_l = c.recv(4).decode('utf-8')
-            s_l = int(s_l)
-            receiver = c.recv(s_l).decode('utf-8')
+            receiver = c.recv(int(s_l)).decode('utf-8')
             m_l = c.recv(16).decode('utf-8')
-            m_l = int(m_l)
-            msg = c.recv(m_l).decode('utf-8')
+            msg = c.recv(int(m_l)).decode('utf-8')
             time = c.recv(8).decode('utf-8')
             print(receiver, '-' * 5, msg, '-' * 5, time)
             print()
         elif person == '3':
             msg = c.recv(17).decode('utf-8')
+            time = c.recv(8).decode('utf-8')
+            print("status : ", msg, '-'*5, time)
+            print()
+        elif person == '4':
+            msg = c.recv(21).decode('utf-8')
             time = c.recv(8).decode('utf-8')
             print("status : ", msg, '-'*5, time)
             print()
@@ -97,18 +107,15 @@ def client_send(receiver, msg):
     send_message = msg.encode('utf-8')
     receiver = receiver.encode('utf-8')
     sender = name.encode('utf-8')
-    msg_length = len(send_message)
-    msg_length = str(msg_length).encode('utf-8')
+    msg_length = str(len(send_message)).encode('utf-8')
     msg_length += b' ' * (16 - len(msg_length))
     c.send(msg_length)
     c.send(send_message)
-    sender_length = len(sender)
-    sender_length = str(sender_length).encode('utf-8')
+    sender_length = str(len(sender)).encode('utf-8')
     sender_length += b' ' * (4 - len(sender_length))
     c.send(sender_length)
     c.send(sender)
-    receiver_length = len(receiver)
-    receiver_length = str(receiver_length).encode('utf-8')
+    receiver_length = str(len(receiver)).encode('utf-8')
     receiver_length += b' ' * (4 - len(receiver_length))
     c.send(receiver_length)
     c.send(receiver)
@@ -119,6 +126,7 @@ SERVER = socket.gethostbyname(socket.gethostname())
 server_messages = []                                # [[sender, receiver , message, time],......]
 conn_name_dict = {}                                 # {(connection : sender_name)......}
 conn_ip_dict = {}                                   # {(connection : ip_address).......}
+online = set()                                      # {online-user_names,.....}
 
 
 ch = int(input("1. Server\n2. Client\n"))
@@ -136,10 +144,14 @@ else:
     th = threading.Thread(target=client_receive)
     th.start()
     name = (input("Enter Your Name : ")).upper()
+    notice_length = str(len(name)).encode('utf-8')
+    notice_length += b' ' * (4 - len(notice_length))
+    c.send(notice_length)
+    c.send(name.encode('utf-8'))
     print("[CONNECTED] TO SERVER : {} AS {}".format(SERVER, name))
 
     while 1:
-        print("Do you want to ::\n1.Broadcast\n2.Personal")
+        print("\nDo you want to ::\n1.Broadcast\n2.Personal")
         print("To quit using Broadcast or Personal, type \"quit\"")
         option = int(input("Enter your Option : "))
         if option == 1:
